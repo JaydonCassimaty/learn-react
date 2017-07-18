41719ff7d0524e4a6bd27feee55cac384c9960af
Jaydon Cassimaty <jaydoncassimaty@gmail.com>
2017-07-18 21:01:03 +1000

    Site Structure Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to react</h2>
-        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Stuff and things
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header';
+import Clock from './Clock';
+import FilterableProductTable from './FilterableProductTable';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'},
+  {category: 'Electronics', price: '$My.Dude', stocked: true, name: 'Philip'}
+];
+
+ReactDOM.render(
+  <Header/ > ,
+  document.getElementById('header')
+);
+
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS}/ > ,
+  document.getElementById('root')
+);
+//
+// ReactDOM.render(
+//   <Clock / > ,
+//   document.getElementById('clock')
+// );
+
 registerServiceWorker();
