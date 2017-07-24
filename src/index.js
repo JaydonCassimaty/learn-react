82dc7378d578912aabb0f51b0a10fcd180b82e8e
Jaydon Cassimaty <jaydoncassimaty@gmail.com>
2017-07-24 18:02:44 +1000

    Another fix...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'index.css';
 import 'containers/app/Animations.css';
 import App from 'containers/app/App';
-import registerServiceWorker from 'registerServiceWorker';
+import registerServiceWorker from 'utils/registerServiceWorker';
 
 ReactDOM.render(
   <App/ > ,
