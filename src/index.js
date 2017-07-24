bb7fe2a64e72c3f7712dc98ae3be7460d317c60d
Jaydon Cassimaty <jaydoncassimaty@gmail.com>
2017-07-24 17:46:44 +1000

    stuff

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.css';
 import 'containers/app/Animations.css';
 import App from 'containers/app/App';
-import registerServiceWorker from 'utils/registerServiceWorker';
+import registerServiceWorker from 'registerServiceWorker';
 
 ReactDOM.render(
   <App/ > ,
